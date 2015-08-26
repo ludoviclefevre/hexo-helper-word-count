5afd77f80d3245dd35623ab652f3c24e39815d21
Ludovic LEFEVRE <contact@ludoviclefevre.fr>
2015-08-26 14:44:00 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,4 @@
+(function () {
+    'use strict';
+    hexo.extend.helper.register('wordcount', require('./lib/helper'));
+})();
---
+++ test/test.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    var path = require('path'),
+        chai = require('chai'),
+        assert = chai.assert,
+        Promise = require('bluebird'),
+        fs = Promise.promisifyAll(require('fs')),
+        helper = require('../lib/helper');
+
+    var readFileOptions = {
+        encoding: 'utf8'
+    };
+
+    var getContent = function () {
+        var filePath = path.join(__dirname, 'fixtures', '200Words.txt');
+        return fs.readFileAsync(filePath, readFileOptions);
+    };
+
+    var wordCount = function (content) {
+        return helper(content);
+    };
+
+    var assertHas200Words = function (result) {
+        assert.equal(result, 200);
+    };
+
+    describe('Hexo Helper Word Count', function () {
+        it('should find 200 words', function () {
+            return getContent()
+                .then(wordCount)
+                .then(assertHas200Words);
+        });
+    });
+})();
